4fced785495665be6e06ac9b693db5fd9d5da3ba
Daniel Allen <dan@aeternagroup.net>
2026-01-11 19:30:38 +0000

    Update to schema version v4.57.0

---
+++ variables.tf
@@ -0,0 +1,39 @@
+variable "storage_shares" {
+  description = <<EOT
+Map of storage_shares, attributes below
+Required:
+    - name
+    - quota
+Optional:
+    - access_tier
+    - enabled_protocol
+    - metadata
+    - storage_account_id
+    - storage_account_name
+    - acl (block):
+        - access_policy (optional, block):
+            - expiry (optional)
+            - permissions (required)
+            - start (optional)
+        - id (required)
+EOT
+
+  type = map(object({
+    name                 = string
+    quota                = number
+    access_tier          = optional(string)
+    enabled_protocol     = optional(string, "SMB")
+    metadata             = optional(map(string))
+    storage_account_id   = optional(string)
+    storage_account_name = optional(string)
+    acl = optional(object({
+      access_policy = optional(object({
+        expiry      = optional(string)
+        permissions = string
+        start       = optional(string)
+      }))
+      id = string
+    }))
+  }))
+}
+
